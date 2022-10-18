7490a2c22794d8cf5a1a8cd53974def9bbff986c
Anigioro-Ezekiel <anigioroea@gmail.com>
2022-10-17 22:01:27 -0700

    update

--- _printf.c
+++ _printf.c
@@ -2,11 +2,14 @@
 
 void print_buffer(char buffer[], int *buff_ind);
 
+
+
 /**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
+
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -38,17 +41,14 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+					flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
 		}
 	}
-
 	print_buffer(buffer, &buff_ind);
-
 	va_end(list);
-
 	return (printed_chars);
 }
 
